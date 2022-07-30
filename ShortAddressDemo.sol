c0a0ec864db78bda4a128cfe5dc8e286ff421e1d
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-30 11:47:31 -0400

    Update ShortAddressDemo.sol

--- ShortAddressDemo.sol
+++ ShortAddressDemo.sol
@@ -2,4 +2,9 @@ pragma solidity ^0.8.2;
 
 contract ShortAddressDemo {
     mapping (address => uint256) public balances;
+
+    constructor() public {
+        balances[msg.sender] = 1000000000000000000;
+    }
+
 }
