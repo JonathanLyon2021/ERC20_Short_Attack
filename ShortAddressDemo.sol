62f695be390b3be42bf4b2b4fd64a239948790be
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-30 11:53:01 -0400

    Update ShortAddressDemo.sol
    Added public method/function transfer.

--- ShortAddressDemo.sol
+++ ShortAddressDemo.sol
@@ -7,4 +7,12 @@ contract ShortAddressDemo {
         balances[msg.sender] = 1000000000000000000;
     }
 
+    function transfer(address recipient, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Not enough Balance");
+        require(balances[msg.sender] - amount < balances[msg.sender], "Underflow");
+        require(balances[recipient] + amount > balances[recipient], "Overflow");
+        balances[msg.sender] -= amount;
+        balances[recipient] += amount;
+    }
+
 }
